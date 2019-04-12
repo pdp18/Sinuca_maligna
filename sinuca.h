66acc9a8059e0773684189bafbbe6b185cd0e62e
Useavarinha <49572709+Useavarinha@users.noreply.github.com>
2019-04-12 19:21:06 -0300

    Update sinuca.h

--- sinuca.h
+++ sinuca.h
@@ -1,3 +1,42 @@
 #pragma once
 
+#include <stdlib.h>
+#include <stdio.h>
 
+typedef struct
+{
+  float x;
+  float y;
+}Vetorf;
+
+typedef struct No
+{
+  float raio;
+  Vetorf vel;
+  Vetorf pos;
+  
+  struct No* prox;
+}Bola;
+
+void copiaVetorf(Vetorf* copia, Vetorf* original)
+{
+  copia->x = original->x;
+  copia->y = original->y;
+}
+
+Bola* insereBola(Bola* b, float raio, Vetorf* vel, Vetorf* pos)
+{
+  if (bola == NULL)
+  {
+    b = (Bola*) malloc(sizeof(Bola));
+    b->raio = raio;
+    copiaVetorf(b->pos, pos);
+    copiaVetorf(b->vel, vel);
+    b->prox = NULL;
+  }
+  
+  else
+    b->prox = insereBola(b->prox, raio, vel, pos);
+  
+  return b;
+}

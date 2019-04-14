d859de449e7aeeb0c4189b7c8256e10f57e15fc1
Useavarinha <marcosrenanmesquitacorreia@gmail.com>
2019-04-14 12:10:04 -0300

    ajeitando a main

--- main.c
+++ main.c
@@ -8,31 +8,22 @@ int main()
     Vetorf vel;
     Vetorf pos;
     Bola* b = NULL;
-<<<<<<< HEAD
     b = insereBola();
-=======
     b = insereBola()
-
-    ///
->>>>>>> 0375b631725f440ecbdf78014b911090ee03227f
     printf("Qual a quantidade de bolas ? ");
     scanf("%d",&n_bolas);
     printf("Qual o raio da bola N:%d ",cont);
     scanf("%f",&raio);
     printf("Qual o velocidade da bola N:%d ",cont);
     scanf("%f%f",&vel.x,&vel.y);
-    ///
     while(n_bolas>cont)
     {
         b=insereBola(b,raio,&vel,&pos);
         cont++;
-<<<<<<< HEAD
         b=insereBola(b,raio,&vel,);
         velocidade=0;
         raio=0;
         n_bolas--;
-=======
->>>>>>> 0375b631725f440ecbdf78014b911090ee03227f
     }
     return 0;
 }

0375b631725f440ecbdf78014b911090ee03227f
ClebinhoRainbow <cledsonaraujo@alunos.utfpr.edu.br>
2019-04-14 11:54:18 -0300

    tirei os trocos do loop

--- main.c
+++ main.c
@@ -3,25 +3,25 @@
 #include "Bola.h"
 int main()
 {
-    int n_bolas,cont=1;
+    int n_bolas,cont=0;
     float raio ;
     Vetorf vel;
+    Vetorf pos;
     Bola* b = NULL;
     b = insereBola()
+
+    ///
     printf("Qual a quantidade de bolas ? ");
     scanf("%d",&n_bolas);
-    while(n_bolas>0)
+    printf("Qual o raio da bola N:%d ",cont);
+    scanf("%f",&raio);
+    printf("Qual o velocidade da bola N:%d ",cont);
+    scanf("%f%f",&vel.x,&vel.y);
+    ///
+    while(n_bolas>cont)
     {
-        printf("Qual o raio da bola N:%d ",cont);
-        scanf("%f",&raio);
-        printf("Qual o velocidade da bola N:%d ",cont);
-        scanf("%f%f",&vel.x,&vel.y);
+        b=insereBola(b,raio,&vel,&pos);
         cont++;
-        b=insereBola(b,raio,&vel,);
-        velocidade=0;
-        raio=0;
-        n_bolas--;
-
     }
     return 0;
 }
